a10cb15dc4cca705ca2d893b48304eab9bd77c30
drakrig <drakrig@gmail.com>
2024-04-21 16:58:13 +0300

    Code refactoring:
    1. Add annotation to all module functions
    2. Remove unnecessary cast to numpy.ndarray

--- maze.py
+++ maze.py
@@ -3,41 +3,41 @@ import matplotlib.pyplot as plt
 import gymnasium as gym
 from gymnasium import spaces
 import pygame
-from typing import TYPE_CHECKING, Optional
+from typing import TYPE_CHECKING, Optional, Dict, Tuple, List, Type, Any
 
 class StateV3():
     """
     Maze states representation class. Store information about state coords, type, reward and neighobiurs
     """
-    def __init__(self, coords, neighbours, valid_actions, state_type="normal"):
-        self._coords = np.array(coords)
+    def __init__(self, coords: np.ndarray, neighbours:Dict , valid_actions:List[int], state_type: str="normal"):
+        self._coords = coords
         self.type = state_type
         self.reward = 0
         self.neighbours = neighbours
         self.valid_actions = valid_actions
 
     @property
-    def coords(self):
+    def coords(self) -> np.ndarray:
         """Return state coords as NumPy array"""
         return self._coords
     
     @property
-    def observation(self):
+    def observation(self) -> np.ndarray:
         """Return state existing neighbours as bool array in format [N, E, S, W]"""
         neighbours = [True if neighbour is not None else False for neighbour in self.neighbours.values()]
         return np.array(neighbours, dtype=np.bool_)
 
     @property
-    def xy(self):
+    def xy(self) -> Tuple[int,int]:
         """Return state coords as tuple"""
         return tuple(self.coords)
 
-    def get_valid_neighbours(self):
+    def get_valid_neighbours(self) -> np.ndarray:
         """Return state neighbours"""
         neighbours = np.array((list(self.neighbours.values())))
         return neighbours[neighbours != None]
     
-    def add_neighbour(self, action, neighbour):
+    def add_neighbour(self, action:int, neighbour:'StateV3') -> None:
         """Add neighbour to state"""
         self.neighbours[action] = neighbour
         self.valid_actions.remove(action)
@@ -51,7 +51,12 @@ class MazeBuilderV4():
 * fullfill: default 0.5. Determine what percent of maximum possible total connections will be used during creation with respect to max connection per state. For example, for maze of size (10,10) with connection_limit = 4 max amount of connection is 180. If fullfill = 0.5 then we will have 180 * 0.5 = 90 connections in maze. 
 * seed: random seed for creation, so we can control the process
     """
-    def __init__(self, actions, shape=(10,10), connections_limit=3, fullfill=0.5,seed=None):
+    def __init__(self, 
+                 actions:Dict, 
+                 shape: Tuple[int,int]=(10,10), 
+                 connections_limit:int=3, 
+                 fullfill:float=0.5, 
+                 seed: int=None):
         self.movements = actions
         self.shape = shape
         self.size = shape[0] * shape[1]
@@ -85,16 +90,16 @@ class MazeBuilderV4():
         self.filled_pathes=0
         self._create_maze()
     
-    def _is_full_enoght(self):
+    def _is_full_enoght(self) -> bool:
         return self.filled_pathes < self.total_pathes * self.fullfill
 
     #State Section
 
-    def _create_state(self, coords):
+    def _create_state(self, coords:np.ndarray) -> Type[StateV3]:
         neighbours, actions = self._find_neighbours(coords)
         return StateV3(coords, neighbours, actions)
     
-    def _if_state_exist(self, coords):
+    def _if_state_exist(self, coords:Tuple[int,int]) -> bool:
         try:
             self.states[coords]
             return True
@@ -103,35 +108,35 @@ class MazeBuilderV4():
 
     #Action Section
     
-    def _if_valid_action(self, coords, action):
+    def _if_valid_action(self, coords:np.ndarray, action:int) -> bool:
         next_coords = coords + action
         for i in range(len(self.shape)):
             if not 0<next_coords[i]<=self.shape[i]:
                 return False
         return True
     
-    def _opposite_direction(self, action):
+    def _opposite_direction(self, action:int) -> int:
         return (action + len(self.movements)/2) % len(self.movements)
     
     # Neighbours Section
 
-    def _find_neighbours(self, coords):
+    def _find_neighbours(self, coords:np.ndarray) -> Tuple[Dict, List[int]]:
         valid_directions = dict()
         valid_actions = list()
         for key, action in self.movements.items():
-            next_coords = np.array(coords) + action
+            next_coords = coords + action
             valid = self._if_valid_action(coords, action)
             valid_directions[key] = None
             if valid:
                 valid_actions.append(key)
         return valid_directions, valid_actions
     
-    def _connect_states(self,action, state, next_state):
+    def _connect_states(self,action:int, state:StateV3, next_state:Type[StateV3]) -> None:
         state.add_neighbour(action, next_state)
         opposite = int(self._opposite_direction(action))
         next_state.add_neighbour(opposite, state)
     
-    def _create_maze(self):
+    def _create_maze(self) -> None:
         """Class main loop"""
         limit = 10000
         self._step = 0
@@ -140,7 +145,7 @@ class MazeBuilderV4():
             if self.seed:
                 self.seed+=1
     
-    def _create_path(self, state):
+    def _create_path(self, state:Type[StateV3]) -> None:
         if self.seed:
             np.random.seed(self.seed)
         if state.get_valid_neighbours().shape[0] == self.connections_limit\
@@ -190,15 +195,15 @@ class MazeV5(gym.Env):
 * render_mode str: optional, possible values [human, rgb_array]. Determine how to handle rendered maze
     """
     def __init__(self,
-                shape=(10,10),
-                connections_limit=3,
-                fullfill=0.5,
-                traps_percent=0,
-                distance_quantile=0.75,
-                dynamic_reward=True,
-                seed=None,
-                render=False,
-                render_mode: Optional[str] = None):
+                shape: Tuple[int,int]|None = (10,10),
+                connections_limit: int|None = 3,
+                fullfill: float|None = 0.5,
+                traps_percent: float|None = 0,
+                distance_quantile: float|None = 0.75,
+                dynamic_reward: bool|None = True,
+                seed: int|None = None,
+                render: bool|None = False,
+                render_mode: str|None = None):
         super(MazeV5, self).__init__()
         self.movements = {0:np.array([0,1]), 1:np.array([1,0]), 2:np.array([0,-1]), 3:np.array([-1,0])}
         builder = MazeBuilderV4(self.movements, shape, connections_limit, fullfill, seed)
@@ -234,7 +239,7 @@ class MazeV5(gym.Env):
                 self.screen = pygame.display.set_mode((640, 640))
             self.render()
     
-    def calc_direction(self): #v5
+    def calc_direction(self) -> float: #v5
             ax, ay = np.array([0, 1]), np.array([1, 0])
             vector = self.goal - self.current_state.coords
             if vector.sum()==0:
@@ -245,14 +250,14 @@ class MazeV5(gym.Env):
                                     np.degrees(np.arccos(cos_y))])
             return angle_degrees / 180
 
-    def _calc_step_reward(self):
+    def _calc_step_reward(self) -> float:
         distance_reward = -0.01 * (self._calc_distance(self.goal, self.current_state.coords) / self.max_distance)
         return distance_reward + self.current_state.reward
     
-    def _calc_distance(self, point1, point2):
+    def _calc_distance(self, point1:np.ndarray, point2:np.ndarray) -> float:
         return np.sqrt(np.power(point1 - point2, 2).sum())
 
-    def _is_valid_action(self, movement):
+    def _is_valid_action(self, movement:np.ndarray) -> bool:
         possible_coords=[]
         for key in self.current_state.neighbours.keys():
             if self.current_state.neighbours[key] is not None:
@@ -260,7 +265,7 @@ class MazeV5(gym.Env):
         new_coords = tuple(self.current_state.coords + movement)
         return new_coords in possible_coords
     
-    def _place_goal(self, points):
+    def _place_goal(self, points:np.ndarray) -> None:
         distances, pathes = np.array([]), np.zeros((1,2))
         for i in range(points.shape[0]):
             distance = self._calc_distance(self.entrance, points[i])
@@ -278,7 +283,7 @@ class MazeV5(gym.Env):
         self.states[tuple(self.goal)].type = "treasure"
         self.states[tuple(self.goal)].reward = self._calc_distance(self.entrance, self.goal) if self.dynamic_reward else self.shape[0]*self.shape[1]
 
-    def _place_special_points(self, traps_percent):
+    def _place_special_points(self, traps_percent:float) -> None:
         points = list(self.states.keys())
         points.remove(self.current_state.xy)
         limit = np.round(len(points) * traps_percent)
@@ -293,19 +298,19 @@ class MazeV5(gym.Env):
         if self.goal is None:
             self._place_goal(np.array(points))
 
-    def get_observation(self):
+    def get_observation(self) -> np.ndarray:
         distance = self._calc_distance(self.current_state.coords, self.goal) / self.max_distance
         angel = self.calc_direction() #v5
         observation = np.append(np.append(distance, angel), self.current_state.observation)
         return observation
     
-    def reset(self, seed=None, options=None):
+    def reset(self, seed:int | None = None, options: dict[str, Any] | None = None) -> Tuple[np.ndarray, dict[str, Any]]:
         self.current_state = self.states[tuple(self.entrance)]
         if self.use_render:
             self.render()
         return self.get_observation(), {}
     
-    def step(self, action):
+    def step(self, action:int) -> Tuple[np.ndarray, float, bool, bool, Dict]:
         # Take a step with the given action and return the next state, reward, done, and info
         valid_action = self._is_valid_action(self.movements[action])
         if valid_action:
@@ -318,9 +323,9 @@ class MazeV5(gym.Env):
             reward = self._calc_step_reward() * (2**self._repeat) if self.dynamic_reward else -1
         done = all(self.current_state.coords == self.goal)
         observation = self.get_observation()
-        return observation, reward, done, False,dict()
+        return observation, reward, done, False, dict()
 
-    def _draw_state_neighbours(self, state, center, deep):
+    def _draw_state_neighbours(self, state:StateV3, center:np.ndarray, deep:int) -> None:
         if deep >= self._render_deep:
             return
         for neighbour in state.neighbours.values():
@@ -331,7 +336,7 @@ class MazeV5(gym.Env):
                 pygame.draw.circle(self.surface, self._colors[neighbour.type] ,coords, self._render_state_size)
                 self._draw_state_neighbours(neighbour, coords, deep+1)
     
-    def render(self):
+    def render(self) -> np.ndarray|None:
         #Main screen
         self.surface.fill("grey")
         self._draw_state_neighbours(self.current_state, np.array([320,320]),0)
@@ -357,7 +362,7 @@ class MazeV5(gym.Env):
                     np.array(pygame.surfarray.pixels3d(self.surface)), axes=(1, 0, 2)
                 )
 
-    def _plot_maze(self, figsize=(5,5), save=False):
+    def _plot_maze(self, figsize: Tuple[int.int] = (5,5), save: bool|None = False) -> None:
         plt.figure(figsize=figsize)
 
         for state in self.states.values():
